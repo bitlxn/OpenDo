59c0878911f59c0bfb29c1cfeafec51e434fd574
bietlxn <blixtengaming@gmail.com>
2021-12-27 14:00:06 +0100

    published

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # OpenDo
 A fully open source, modular, customizable and private todo list app. You decide what you want public, you decide how you want to store your tasks. We belive you should decide what you want.
 
+## Features
+* Fully open source
+* Fully modular
+* Fully customizable
+* Fully private
+* Fully secure
+* Your files your data
+* No data tracking
+
+## How it works
+You import a empty .txt(more will come) file. It reads the file and with a nice user interface you can add, edit, delete and mark tasks. You can also add a description to your tasks. The user interface is very simple and easy to use, you will be able to create and download your own themes. 
+
 ---
 
 # Installing
@@ -32,6 +44,10 @@ After cloning the app you can go to the directory of the app with the following 
 ```bash
 cd OpenDo
 ```
+You may need to run this command before you can run the app:
+```bash
+npm install
+```
 To run the app you can use the following command:
 ```bash
 npm run dev
@@ -44,14 +60,38 @@ npm run build
 ---
 
 # Special Links
-This app is hosted on github, so you can find the source code and the latest version of the app on github. You can also find the latest version of the app on the offical website here: https://opendo.bietlxn.com/
+This app is hosted on github, so you can find the source code and the latest version of the app on github. You can also find the latest version of the app on the offical website here: 
+* **not available yet.**
 
 **Note:** If you are not using the app on the offical website we do not have have any way to help you with the app. We are not responsible for any bugs or issues that may occur when using the app on an unofficial website. We are not responsible for anything happening to your computer or your data, when you download something from an unofficial website(we recommend not to download something thats not verified).
 
-**Verified Websites**
-* [opendo.bietlxn.com](https://opendo.bietlxn.com/)
+**Verified Websites:**
 
 ---
+
+# Versions
+This app is currently in beta and is not fully functional. We are working on it and will update it as soon as possible. We update the app as much as we can.
+
+current version: 0.0.1
+version history:
+* 0.0.1:
+    * initial release of the app and the first version of the app.
+    * the app is not fully functional. The app is currently unuseable.
+    * the app is fully open source. It was made public on github, Monday, 27 December 2021.
+
+---
+
+# Contact
+If you have any questions or comments about this app or want to report a bug or have a suggestion please contact me at:
+* hello@bietlxn.com
+* https://twitter.com/bietlxn
+
+you can also learn about me on my website:
+* https://bietlxn.com
+
+---
+
+# License
 
 This app uses the MIT license. You can find the license here:
 https://github.com/bietlxn/OpenDo/blob/main/LICENSE
--- src/App.svelte
+++ src/App.svelte
@@ -1,38 +1,57 @@
 <script>
-	let fileLocation = "Browse Location";
-	let fileName = "";
+	console.log("hello");
 
-	function browseResult(){
-		var fileInput = document.getElementById('fileInput');
-		fileLocation = fileInput.value;
-		console.log(fileLocation);
-	}
+	let filePath = "import";
+	let fileName = "unnamed";
 
-	function createFile() {
-		const fileNameHtml = document.getElementById("fileName");
-		// if something is typed in the input box
-		if (fileNameHtml.value != "") {
-			document.getElementById("needName").style.display = "none";
+	let canCreate = false;
+	let isInFile = false;
 
-
+	function browse() {
+		console.log("browse");
+		filePath = document.getElementById("fileUpload").value;
+		console.log(filePath);
+		// if filePath is not empty or "Browse", then canCreate is true
+		if (filePath != "Browse" && filePath != "") {
+			canCreate = true;
+			document.getElementById("pleaseImport").style.display = "none";
 		}
 		else {
-			document.getElementById("needName").style.display = "flex";
+			canCreate = false;
+		}
+	}
 
-
-			return;
+	function openProject() {
+		console.log("openProject");
+		if (canCreate) {
+			isInFile = true;
+		}
+		else {
+			console.log("cannotCreate");
+			document.getElementById("needFile").style.display = "flex";
+			isInFile = false;
 		}
 	}
 </script>
 
 <div class="App" id="App">
-	<div class="createProject">
-		<input type="text" name="fileNameInputName" class="fileNameInput" id="fileName" placeholder="type name of project here">
-		<input type="file" onchange="browseResult(event)" id="fileInput" class="fileInput">
-		<button class="createProjectButton" on:click={createFile}>Create <span class="hideProject">Project</span></button>
-		<span class="needName" id="needName">You need a name, please enter a name for your project.</span>
-		<span class="importFile">import a file</span>
-	</div>
+	{#if isInFile == true}
+		<div class="projectFile" id="projectFile" style="text-align: center;">
+			<h1 style="color: white;">{fileName}</h1>
+			<h2 style="color: white;">{filePath}</h2>
+		</div>
+	{:else}
+		<div class="importProject" id="importProject">
+			<span class="pleaseImport" id="pleaseImport">please import a file</span>
+			<label for="fileUpload" class="customFileUpload">
+				{filePath}
+			</label>
+			<input id="fileUpload" type="file" on:change={browse}/>
+			<button class="importProjectButton" on:click={openProject}>open <span class="hideProject">project</span></button>
+			<span class="needFile" id="needFile">you need a file, please enter a file for your project. <br> if you dont know how to create a file please click "learn more".</span>
+			<span class="learnMore">learn more</span>
+		</div>
+	{/if}
 </div>
 
 <style>
@@ -46,7 +65,7 @@
 		align-items: center;
 	}
 
-	.createProject {
+	.importProject {
 		width: 100%;
 		height: 100%;
 		display: flex;
@@ -56,60 +75,11 @@
 		text-align: center;
 	}
 
-	.createProject .fileNameInput {
-		width: 15%;
+	.importProject .importProjectButton {
 		text-align: center;
-		height: 50px;
-		border: none;
-		border-bottom: 1px solid rgba(255, 255, 255, 0.13);
-		background: transparent;
-		color: rgb(255, 255, 255);
-		font-size: 20px;
-		font-family: 'Roboto', sans-serif;
-		outline: none;
-	}
-
-	.createProject .fileNameInput:focus {
-		border-bottom: 1px solid rgba(255, 255, 255, 0.452);
-	}
-
-	.createProject .fileNameInput:placeholder-shown {
+		background: rgba(255, 145, 0, 0.171);
 		color: rgba(255, 255, 255, 0.466);
-	}
-
-	.createProject .fileInput {
-		background: transparent;
-		color: rgba(255, 255, 255, 0.466);
-		border: solid 1px rgba(255, 255, 255, 0.13);
-		border-radius: 5px;
-		outline: none;
-		width: 15%;
-		cursor: pointer;
-	}
-
-	.createProject .fileInput:hover {
-		background: rgba(255, 255, 255, 0.2);
-		color: rgb(255, 255, 255);
-		box-shadow: 0 10px 17px rgba(0, 0, 0, 0.233);
-	}
-
-	.createProject .fileInput:active {
-		background: rgba(255, 255, 255, 0.466);
-		color: rgb(255, 255, 255);
-		box-shadow: 0 0px 0px rgba(0, 0, 0, 0.233);
-	} 
-
-	.createProject .fileInput:focus {
-		background: rgba(255, 255, 255, 0.308);
-		color: rgba(255, 255, 255, 0.836);
-		box-shadow: 0 0px 0px rgba(0, 0, 0, 0.233);
-	} 
-
-	.createProject .createProjectButton {
-		text-align: center;
-		background: rgba(255, 255, 255, 0.13);
-		color: rgba(255, 255, 255, 0.466);
-		border: solid 1px rgba(255, 255, 255, 0.13);
+		border: solid 1px rgba(255, 145, 0, 0.171);
 		border-radius: 5px;
 		font-size: 20px;
 		font-family: 'Roboto', sans-serif;
@@ -118,30 +88,56 @@
 		cursor: pointer;
 	}
 
-	.createProject .createProjectButton:hover {
-		background: rgba(255, 255, 255, 0.2);
+	.importProject .importProjectButton:hover {
+		background: rgba(255, 145, 0, 0.37);
 		color: rgb(255, 255, 255);
 		box-shadow: 0 10px 17px rgba(0, 0, 0, 0.233);
 	}
 
-	.createProject .createProjectButton:active {
-		background: rgba(255, 255, 255, 0.466);
+	.importProject .importProjectButton:active {
+		background: rgba(255, 174, 68, 0.5);
 		color: rgb(255, 255, 255);
 		box-shadow: 0 0px 0px rgba(0, 0, 0, 0.233);
 	}
 
-	.createProject .importFile {
+	.importProject input[type="file"] {
+		display: none;
+	}
+
+	.importProject .customFileUpload {
+		max-width: 15%;
+		text-align: center;
+		background: rgba(255, 255, 255, 0.082);
+		color: rgba(255, 255, 255, 0.37);
+		border: solid 1px rgba(255, 255, 255, 0.082);
+		border-radius: 5px;
+		font-size: 1.05vw;
+		font-family: 'Roboto', sans-serif;
+		outline: none;
+		width: 15%;
+		padding: 7.5px 0px;
+		cursor: pointer;
+		margin: 10px;
+	}
+	
+	.importProject .customFileUpload:hover {
+		background: rgba(255, 255, 255, 0.24);
+		color: rgb(255, 255, 255);
+		box-shadow: 0 10px 17px rgba(0, 0, 0, 0.233);
+	}
+
+	.importProject .learnMore {
 		color: rgba(255, 255, 255, 0.267);
 		font-size: 18px;
 		cursor: pointer;
 	}
 
-	.createProject .importFile:hover {
+	.importProject .learnMore:hover {
 		color: rgba(255, 255, 255, 0.801);
 		text-decoration: underline;
 	}
 
-	.createProject .needName {
+	.importProject .needFile {
 		position: relative;
 		display: none;
 		color: rgba(255, 124, 124, 0.514);
@@ -149,14 +145,14 @@
 		cursor: pointer;
 	}
 
+	.importProject .pleaseImport {
+		color: rgba(255, 255, 255, 0.267);
+		font-size: 17px;
+	}
+
 	@media only screen and (max-width: 1635px) {
-		.createProject .fileNameInput:placeholder-shown {
-			color: transparent;
-		}
-		
-		.createProject .hideProject {
+		.importProject .hideProject {
 			display: none;
 		}
 	}
-
 </style>
